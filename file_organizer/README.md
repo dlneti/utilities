68c33c5258f9435bddafde78bf1446cfd13751ef
Filip <filip.sterba@gmail.com>
2020-04-16 12:45:56 +1000

    comments

--- file_organizer/README.md
+++ file_organizer/README.md
@@ -1,4 +1,5 @@
 ## Quick and dirty script to organize files in selected folder.
+(for now for MAC only)
 
 - Organizes files into "Videos" and "Pictures" directories based on their mimetype.
 - This script can be expanded by adding more mimetypes and their respective folder names in `TYPE_DIRNAME_MAP`
--- file_organizer/organize.py
+++ file_organizer/organize.py
@@ -7,6 +7,7 @@ selected_path = easygui.diropenbox(default='~/Desktop')
 
 files_in_selected_dir = os.listdir(selected_path)
 
+# map that holds names of directories based on their mimetype + data to be added to new directories later
 TYPE_DIRNAME_MAP = {
     "image": {
         "dirname": "Pictures",
@@ -21,16 +22,20 @@ TYPE_DIRNAME_MAP = {
 for file in files_in_selected_dir:
     if file == ".DS_Store": continue
 
+    ## get the mimetype of file
     mimetype = mimetypes.guess_type(f"{selected_path}/{file}")[0]
 
     if not mimetype:
         print(file)
         continue
 
+    ## name of mimetype
     mimetype = mimetype.split('/')[0]
 
+    ## if mimetype is not in our map we are skipping it
     if (mimetype not in TYPE_DIRNAME_MAP.keys()): continue
 
+    ## create list of files to be added to new directory
     TYPE_DIRNAME_MAP[mimetype]["data"].append(file)
 
 
@@ -41,6 +46,7 @@ for key, value in TYPE_DIRNAME_MAP.items():
         print(f"Directory {value['dirname']} exists! Skipping ...")
         pass
 
+    ## move the file
     for file in value['data']:
         os.rename(f"{selected_path}/{file}",
                   f"{selected_path}/{value['dirname']}/{file}")
